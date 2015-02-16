f052a40c7eafbd4156d1d8942bda3807d34becc0
Alfa59 <d.antoine94@sfr.fr>
2015-02-16 11:51:50 +0100

    Vrai_page_accueil
    Remplacement de la page d’accueil

--- Accueil.php
+++ Accueil.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 	<<!DOCTYPE html>
 	<html>
 		<meta charset="utf-8">
@@ -10,3 +11,32 @@
 
 
 	</html>
+=======
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="author" content="Groupe G10E - ISEP" />
+
+
+        <link rel="stylesheet" href="CSS/style.css" />
+
+        <title> Deal With Eat </title>
+    </head>
+ 
+        
+        <?php include("module/header.php");?>
+        <?php include("module/menu.php");?>
+        
+        
+    <body>
+    <?php include("module/moduleconnect.php");?>
+    <?php include("module/grappe.php");?>
+
+
+    </body>
+
+<?php include("module/footer.php");?>
+        
+</html>
+>>>>>>> Stashed changes
