700bd6ae4d5c4f18e9ecfc221f8c4cd6b92d38a8
olivier madre <olivier.madre@voila.fr>
2015-03-02 19:59:58 +0100

    body and footer
    remplissage du corps et mis en place des logos des réseaux sociaux

--- Accueil.php
+++ Accueil.php
@@ -16,6 +16,27 @@
 
 	<body>
 
+		<!-- Le corps -->	
+
+		<p>
+        Le terme désigne aussi par métonymie les plantes potagères cultivées pour la production de légumes.
+
+		Dans le langage culinaire, « légume » s'oppose à « fruit », mais dans certains cas le même produit peut être cuisiné ou consommé soit comme légume soit comme fruit. </br>
+		Il s'oppose aussi à plante condimentaire, dont l'usage culinaire est différent, même si ce sont également des plantes potagères.
+
+		Dans le domaine de la cuisine et de la gastronomie, « légume » peut également avoir une acception plus large, désignant « tout aliment non carné et non sucré accompagnant un plat de viande ou de poisson au cours d'un repas1 ».</br>
+		Ce sont en outre généralement des « fruits » au sens botanique qui constituent l'accompagnement dans ces plats salés particuliers dits « sucrés-salés » (cuisine sucrée-salée).
+
+		Mais le terme « légume » peut aussi avoir un sens plus restreint quand il ne concerne pas certains féculents (pomme de terre, riz…), la viande ou le poisson étant typiquement accompagné de « légume » et de féculent.
+
+		La production des légumes frais destinés à la commercialisation est le fait en France du maraîchage, qui est une branche de l'horticulture, elle même appartenant à l'agriculture. Une partie non négligeable des légumes est produite dans les jardins potagers familiaux et essentiellement autoconsommée.</br>
+		Les légumes destinés à la conservation (appertisation, surgélation) et les légumes secs sont cultivés en plein champ, et leur récolte est le plus souvent mécanisée.
+        </p>
+
+        <img src = "images/legume.jpg" alt ="Photo de légume" />
+
+
+		<!-- Le pied de page -->
 
 	<?php include('php/pied_de_page.php'); ?>
 
--- php/pied_de_page.php
+++ php/pied_de_page.php
@@ -1,5 +1,9 @@
 <footer id = "pied_de_page">
-
+	<p> Copyright tout droit reservé</p>
+    <img src = "images/fb.png " alt ="logo de fb" />
+    <img src = "images/twitter.png" alt ="logo de twitter"/>
+    <img src = "images/instagram.png" alt ="logo d'instagram"/>
+    <img src = "images/contact.png"alt ="logo du contact"/>
 
 
 
