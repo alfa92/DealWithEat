0d1cef429f1b8f34a481161c53159f1e4f186c21
olivier madre <olivier.madre@voila.fr>
2015-03-02 20:45:06 +0100

    logo approximativement centré
    je pense pas que j ai utilisé la meilleur méthode
    j'ai rajouter des com

--- php/pied_de_page.php
+++ php/pied_de_page.php
@@ -1,9 +1,11 @@
 <footer id = "pied_de_page">
 	<p> Copyright tout droit reservé</p>
-    <img src = "images/fb.png " alt ="logo de fb" />
-    <img src = "images/twitter.png" alt ="logo de twitter"/>
-    <img src = "images/instagram.png" alt ="logo d'instagram"/>
-    <img src = "images/contact.png"alt ="logo du contact"/>
+	<div class= "reseau_sociaux">
+    	<img class = "fb" src = "images/fb.png " alt ="logo de fb" />
+    	<img class = "twitter" src = "images/twitter.png" alt ="logo de twitter"/>
+    	<img class ="instagram" src = "images/instagram.png" alt ="logo d'instagram"/>
+    	<img class ="contact" src = "images/contact.png"alt ="logo du contact"/>
+    </div>	
 
 
 
