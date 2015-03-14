03cafa0922e5409db610b5b54b801ab1b91d5a5f
Alfa59 <d.antoine94@sfr.fr>
2015-03-14 23:38:45 +0100

    footer

--- DWE/php/pied_de_page.php
+++ DWE/php/pied_de_page.php
@@ -1,14 +1,14 @@
-<footer id="pied_de_page">
-	 <div id="banniere">
-    <p id="copyright"> tout droit réservé copyright </p>
+<footer>
+	 
+    
+    <div id="banniere">
+    <p id="copyright"> Tout droit réservé copyright </p>
+        <i><a>Réglement</a> - <a>Contact</a> - <a>Inscription</a> - <a>FAQ/Aide</a></i>
 	<div class = "reseaux">
 		<img class= "fb" src = "css/images/fb.png" alt = "logo de fb">
 		<img class ="twitter" src = "css/images/twitter.png" alt = "logo de twitter" />
 		<img class ="contact" src = "css/images/contact.png" alt = "logo des contacts" />
-		<img class ="instragram" src = "css/images/instagram.png" alt = "logo d'instagram" />
 	</div>
     
    </div>
-	<!--img id ="banniere" src ="css/images/banniereDWE.jpg" alt = "logo de la banniere" -->
-
 </footer> 
