5608ad3dc1da01f81cdab43ca67e5f8c80506b0c
Alfa59 <d.antoine94@sfr.fr>
2015-03-19 18:30:19 +0100

    panier

--- DWE/panier.php
+++ DWE/panier.php
@@ -15,40 +15,48 @@
 		<?php if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?> 
         </header>
             <body>
+                
                 <table id="panier_panier">
+                    <img src="css/images/picnic5.png" style="width:60px;position:absolute;margin-top:20px;margin-left:10%;">
                     <center id="panier_titre">Votre Panier </center>
-                        <hr/>
+               
                     
                 <tr>
                         <th> Produit </th>
                         <th> Quantité </th>
                         <th> Prix unité </th>
                         <th> Prix </th>
-                        <th style="width:10px;"> Devise </th>
+                      
                 </tr>        
                  <tr>
-                       <td>Banane</td>
+                       <td><img style="width:50px;" src="css/images/banane.jpg">Banane</td>
                        <td> 2kg</td>
                        <td> 12€/kg </td>
-                        <td> 24 </td>
-                     <td style="width:10px;"> € </td>
+                        <td style="color:green;font-weight:500;font-size:22px;"> 24 €</td>
+                    
                    </tr>
                    <tr>
-                       <td>Banane</td>
-                       <td> 2kg</td>
+                       <td> <img style="width:50px;" src="css/images/pommes.jpg">Pommes</td>
+                       <td> 2kg </td>
                        <td> 12€/kg </td>
-                        <td> 24 </td>
-                       <td style="width:10px;"> € </td>
+                        <td style="color:green;font-weight:500;font-size:22px;"> 24 €</td>
+                  
                    </tr>
                     <tr >
-                    <td style="border:none;" colspan="4"> </td>
+                    <td style="border:none;" colspan=""> </td>
                     </tr>
-                    <tr>
-                    <td colspan="3"> Prix total</td>
-                    <td> 48  </td>
-                        <td style="width:10px;"> € </td>
+                    <tr style="border:1px solid black;">
+                    <td colspan="2" style="border:1px solid black;"> Prix total</td>
+                    <td colspan="4" style="color:red;font-weight:500;font-size:24px;"> 48  €</td>
+                       
                 </tr>
 </table>
+                
+                <div id="panier_dessous">
+                    <h2 style="margin-top:20px;"> Retourner faire du shopping </h2>
+                    <h2 style="margin-left:80%;"> Payer </h2>
+                
+                </div>
             </body>
 
 
