bb8a43f806a7124ccf461c9657e38ae7252f6ad1
olivier madre <olivier.madre@voila.fr>
2015-03-19 18:58:22 +0100

    amélioration du footer
    rajout des liens inscription etc

--- DWE/php/pied_de_page.php
+++ DWE/php/pied_de_page.php
@@ -3,7 +3,7 @@
     
     <div id="banniere">
     <p id="copyright"> Tout droit réservé copyright </p>
-        <i><a>Réglement</a> - <a>Contact</a> - <a>Inscription</a> - <a>FAQ/Aide</a></i>
+        <i> <a href =""> Réglement </a> - <a href =""> Contact </a> - <a href= "Inscription.php">Inscription</a> - <a href = "faq.php"> FAQ/Aide </a> </i>
 	<div class = "reseaux">
 		<img class= "fb" src = "css/images/fb.png" alt = "logo de fb">
 		<img class ="twitter" src = "css/images/twitter.png" alt = "logo de twitter" />
