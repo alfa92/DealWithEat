ba2e7059b80b7a42ec2ae9980e22d1e93c23f654
Alfa59 <d.antoine94@sfr.fr>
2015-03-20 11:36:55 +0100

    FAQ

--- DWE/php/config.php
+++ DWE/php/config.php
@@ -9,6 +9,8 @@ espace membre puisse fonctionner correctement.
 
 //On se connecte a la base de donnee
 $conn=mysqli_connect('localhost', 'root', 'root','dealwitheat');
+$conn1=mysqli_connect('localhost', 'root', 'root','mydb');
+mysqli_set_charset($conn1,"UTF8");
 $bdd = new PDO('mysql:host=localhost;dbname=mydb;charset=utf8', 'root', 'root');
 //Email du webmaster
 $mail_webmaster = 'example@example.com';
--- DWE/php/pied_de_page.php
+++ DWE/php/pied_de_page.php
@@ -1,14 +1,14 @@
 <footer>
-	 
-    
-    <div id="banniere">
+   
+        <p id="lien_footer"> <a href =""> Réglement </a> - <a href =""> Contact </a> - <a href= "Inscription.php">Inscription</a> - <a href = "faq.php"> FAQ/Aide </a> </p>
+    <div id="bas_footer">
     <p id="copyright"> Tout droit réservé copyright </p>
-        <i> <a href =""> Réglement </a> - <a href =""> Contact </a> - <a href= "Inscription.php">Inscription</a> - <a href = "faq.php"> FAQ/Aide </a> </i>
 	<div class = "reseaux">
 		<img class= "fb" src = "css/images/fb.png" alt = "logo de fb">
 		<img class ="twitter" src = "css/images/twitter.png" alt = "logo de twitter" />
+        <img class ="google+" src = "css/images/google+.png" alt = "logo de google+" />
 		<img class ="contact" src = "css/images/contact.png" alt = "logo des contacts" />
 	</div>
-    
-   </div>
+    </div>    
+
 </footer> 
