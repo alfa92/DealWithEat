44f9614536a8e78287d37624a247be82dcd25772
AltunerSinan <sinan-selcuk.altuner@isep.fr>
2015-03-24 16:13:58 +0100

    index.php
    index.php fini oklm

---
+++ DWE/index.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+    <meta charset="UTF-8">
+
+    <title>Deal With Eat</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+
+  </head>
+
+  <body id="bodyindex">
+  
+<img id= "fondindex" src="css/images/fondindex.jpg" alt="fond page index" /> 
+
+
+  	<p id="logoindex">  <img src="css/images/logodwe.png" alt="logo dwe"/>
+  	</p>
+  	<h5 id="titreindex">
+			DEAL WITH EAT
+    </h5>
+		
+		
+
+	
+
+  </br>
+
+
+<section> 
+     <article>
+
+        <p id="Slogan">ACHETEZ, VENDEZ, ECHANGER </p> 
+
+
+        <p class="ChoixLangue"> Choisissez votre langage/Choose your language  </p> 
+
+        <p class="Drapeaux"> <a href="accueil.php"><img  src="css/images/drapeauFR.png" alt="drapeau de la France " title="Naviguez sur le site en français" width="20%" height="30%"/> </a>              
+         <img src="css/images/drapeauRU.png" alt="drapeau de la RU " titre="browse the siteweb in English" width="20%" height="30%"/>  </p>
+
+     
+
+    </article>
+
+
+           
+
+ 
+
+
+
+           
+</section>
+
+
+
+ 
+
+  </body>
+
+</html>
