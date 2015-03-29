be9fdb00c0c029d48a9d5784b34be2c75530e2a9
Alfa59 <d.antoine94@sfr.fr>
2015-03-29 16:32:06 +0200

    footer

--- DWE/php/pied_de_page.php
+++ DWE/php/pied_de_page.php
@@ -1,22 +1,14 @@
 <footer>
-<<<<<<< Updated upstream
-	 
-    
-    <div id="banniere">
-    	<p id="copyright"> Tout droit réservé copyright </p>
-        <i> <a href =""> Réglement </a> - <a href =""> Contact </a> - <a href= "Inscription.php">Inscription</a> - <a href = "faq.php"> FAQ/Aide </a> </i>
-=======
    
         <p id="lien_footer"> <a href =""> Réglement </a> - <a href =""> Contact </a> - <a href= "Inscription.php">Inscription</a> - <a href = "faq.php"> FAQ/Aide </a> </p>
     <div id="bas_footer">
     <p id="copyright"> Tout droit réservé copyright </p>
-        <p id="coadmin" style="text-align: right"><i><a href="backoffice/connexion.php"> Connexion administrateur </a></i></p>
->>>>>>> Stashed changes
 	<div class = "reseaux">
 		<img class= "fb" src = "css/images/fb.png" alt = "logo de fb">
 		<img class ="twitter" src = "css/images/twitter.png" alt = "logo de twitter" />
+        <img class="google+" src="css/images/google+.png" alt="logo de google+"/>
 		<img class ="contact" src = "css/images/contact.png" alt = "logo des contacts" />
 	</div>
-    
-   </div>
+    </div>
+    <p id="coadmin"><a href="../backoffice/adminmembre.php">Connexion administrateur</a></p>
 </footer> 
