a91cdd3fa98712a41b0a57f96cdfa03949278a09
Alfa59 <d.antoine94@sfr.fr>
2015-03-31 09:25:22 +0200

    ajout panier 2

---
+++ DWE/panier/addpanier.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 30/03/15
+ * Time: 16:22
+ */
+
+require '_header.php';
+    if(isset($_GET['id'])){
+        $product = $DB->query('SELECT Pr_idProduits FROM produits WHERE Pr_idProduits=:id', array('id' => $_GET['id']));
+        if(empty($product)){
+            die ("Ce produit n'existe pas");
+        }
+
+        $panier->add($product[0]->Pr_idProduits);
+
+        echo 'Le produit a été ajouté à votre panier <a href="javascript:history.back()"> Retourner sur le catalogue</a>';?>
+    <p><a href="../panier.php"> Voir son panier </a></p>
+<?php
+
+    }else{
+        echo("Vous n'avez pas séléctionner de produit à ajouter au panier");
+    }
+
+    ?>
