6cdb19b4119aa285deae038a45c5a2baab4abd13
olivier madre <olivier.madre@voila.fr>
2015-04-04 17:06:41 +0200

    Formulaire de la page vente en html finit
    Le code de la page a été restructuré

--- DWE/vendre.php
+++ DWE/vendre.php
@@ -8,118 +8,93 @@
         <title>Deal With Eat</title>
         
     </head>
-    <div id="body">
-<div id="principal">
+
     <header>
         <?php include('php/config.php'); ?>
         <?php include('php/connexion.php'); ?>
         <?php include('php/header.php'); ?>
-		<?php if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?> 
-			
+    <?php if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?> 
+      
     
     </header>
 
-        <body>
+    <body>
             
-            <h1> Vendre</h1>
-            <h2> Pour mettre en vente un produit veuillez remplir le formulaire ci-dessous</h2>
+    <h1> Vendre</h1>
+        <h2> Pour mettre en vente un produit veuillez remplir le formulaire ci-dessous</h2>
             
-          <!-- PHP -->
+        <!-- PHP -->
          <?php
-function upload($index,$destination,$maxsize=FALSE,$extensions=FALSE)
-{
-   //Test1: fichier correctement uploadé
-     if (!isset($_FILES[$index]) OR $_FILES[$index]['error'] > 0) return FALSE;
-   //Test2: taille limite
-     if ($maxsize !== FALSE AND $_FILES[$index]['size'] > $maxsize) return FALSE;
-   //Test3: extension
-     $ext = substr(strrchr($_FILES[$index]['name'],'.'),1);
-     if ($extensions !== FALSE AND !in_array($ext,$extensions)) return FALSE;
-   //Déplacement
-     return move_uploaded_file($_FILES[$index]['tmp_name'],$destination);
-}
+      function upload($index,$destination,$maxsize=FALSE,$extensions=FALSE)
+        {
+          //Test1: fichier correctement uploadé
+          if (!isset($_FILES[$index]) OR $_FILES[$index]['error'] > 0) return FALSE;
+          //Test2: taille limite
+          if ($maxsize !== FALSE AND $_FILES[$index]['size'] > $maxsize) return FALSE;
+          //Test3: extension
+          $ext = substr(strrchr($_FILES[$index]['name'],'.'),1);
+          if ($extensions !== FALSE AND !in_array($ext,$extensions)) return FALSE;
+          //Déplacement
+          return move_uploaded_file($_FILES[$index]['tmp_name'],$destination);
+        }
  
-//EXEMPLES
-  $upload1 = upload('icone','imageUploades/monicone1',15360, array('png','gif','jpg','jpeg') );
-  $upload2 = upload('mon_fichier','imageUploades/file112',1048576, FALSE );
+          //EXEMPLES
+          $upload1 = upload('icone','imageUploades/monicone1',15360, array('png','gif','jpg','jpeg') );
+          $upload2 = upload('mon_fichier','imageUploades/file112',1048576, FALSE );
  
-  if ($upload1) "Upload de l'icone réussi!<br />";
-  if ($upload2) "Upload du fichier réussi!<br />";
-?>
+            if ($upload1) "Upload de l'icone réussi!<br />";
+            if ($upload2) "Upload du fichier réussi!<br />";
+    ?>
 
             
-          
+      <!-- FORMULAIRE HTML -->  
 
 
-
-
-
-
-
-
-
-
-            <!-- FORMULAIRE EN HTML -->
-
-<?php 
-
-/*création du formulaire pour l'upload d'image*/
-?>
-
-<section>
-  <article>
-    <fieldset>
-      <form id="FormulaireVendre" method="post" action="annonce.php" enctype="multipart/form-data" name="form1">
+    <section>
+      <fieldset>
+        <form id="FormulaireVendre" method="post" action="annonce.php" enctype="multipart/form-data" name="form1">
           <p> Veuillez indiquer si vous vendez des fruits ou des légumes:</p><br />
 
-         <input type="radio" name="TypeProduit" value="fruit" id="fruit" /> <label for="fruit">fruit</label><br />
-         <input type="radio" name="TypeProduit" value="legume" id="legume" /> <label for="legume">légume</label><br /> <br/>
+          <input type="radio" name="TypeProduit" value="fruit" id="fruit" /> <label for="fruit">fruit</label><br />
+          <input type="radio" name="TypeProduit" value="legume" id="legume" /> <label for="legume">légume</label><br /> <br/>
 
-         <label for="fruit"> Quel fruit voulez-vous déposez ? </label> <br/> <!-- on choisit le fruit qu'on dépose -->
-         <select name="fruit" id="fruit"> 
+          <label for="fruit"> Quel fruit voulez-vous déposez ? </label> <br/> <!-- on choisit le fruit qu'on dépose -->
+          <select name="fruit" id="fruit"> 
             <option value="pomme"> Pomme </option>
             <option value="poire"> Poire </option>
             <option value="fraise"> Fraise </option>
-        </select> <br/>
+          </select> <br/>
 
-        <label for="legume"> Quel legume voulez-vous déposez ? </label> <br/> <!-- on choisit le fruit qu'on dépose -->
+          <label for="legume"> Quel legume voulez-vous déposez ? </label> <br/> <!-- on choisit le légume qu'on dépose -->
           <select name="legume" id="legume">
             <option value="brocolis"> Brocolis </option>
             <option value="artichaut"> Artichaut </option>
             <option value="radis"> radis </option>
           </select>  
 
-        <p>Indiquez la quantité que vous vouhaitez en kg ou à la pièce </p> <br/> <!-- on choisit la quantité de fruit qu'on dépose --> 
+          <p>Indiquez la quantité que vous vouhaitez en kg ou à la pièce </p> <br/> <!-- on choisit la quantité de fruit/légume qu'on dépose --> 
           <input type="radio" name="unite" value="kg" id="kg"/> <label for="kg"> kg </label> <br/>
           <input type="radio" name="unite" value="pièce" id="pièce"/> <label for="pièce"> pièce </label> <br/>
-          <input type="number" /><br/>
+          <input type="number" /> <br/>
 
-        <p> Indiquez le prix que vous souhaitez </p> <br/>
+          <p> Indiquez le prix que vous souhaitez </p> <br/><!-- on choisit le prix de fruit/légume qu'on dépose -->
           <input type="number"/> <br/>
+          
+          <label  for="description" > Description du produit </label> <br/> 
+          <textarea name="description" rows="8" cols="45">
+          </textarea> <br/>
 
-        <label for="file">Ajouter une photo :</label>
-        <input type="file" name="fichier" id="fichier" />
-        <input type="submit" name="submit" value="Insérer le fichier" />
+          <label for="file">Ajouter une photo :</label>
+          <input type="file" name="fichier" id="fichier" />
+          <input type="submit" name="submit" value="Insérer le fichier" />
 
-            <input type="submit" value="Valider" />
-          </p>
+          <input type="submit" value="Valider" />
         </form>
-    </fieldset>
+      </fieldset>
+    </section>
 
-
-</article>
-</section>
-
-
-
-
-
- 
-            
-         </body>
-        
-    </div>   
-        </div>
+  </body>
+         
    <?php include('php/pied_de_page.php'); ?>
     
     
