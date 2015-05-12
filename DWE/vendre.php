635a2c393695b90f28a9cbe61bc8e1e36c0bbdec
olivier madre <olivier.madre@voila.fr>
2015-05-12 17:44:12 +0200

    CSS du formulaire
    positionement

--- DWE/vendre.php
+++ DWE/vendre.php
@@ -37,6 +37,7 @@
           <input type="radio" name="TypeProduit" value="fruit" id="fruit" /> <label for="fruit">fruit</label><br />
           <input type="radio" name="TypeProduit" value="legume" id="legume" /> <label for="legume">légume</label><br /> <br/>
 
+          <div class="fruit_legume"
           <label for="fruit"> Quel fruit voulez-vous déposez ? </label> <br/> <!-- on choisit le fruit qu'on dépose -->
           <select name="fruit" id="fruit"> 
             <option value="pomme"> Pomme </option>
@@ -49,24 +50,29 @@
             <option value="brocolis"> Brocolis </option>
             <option value="artichaut"> Artichaut </option>
             <option value="radis"> radis </option>
-          </select>  
+          </select>  <br/>
+        </div>
 
-          <p>Indiquez la quantité que vous vouhaitez en kg ou à la pièce </p> <br/> <!-- on choisit la quantité de fruit/légume qu'on dépose --> 
-          <input type="radio" name="unite" value="kg" id="kg"/> <label for="kg"> kg </label> <br/>
-          <input type="radio" name="unite" value="pièce" id="pièce"/> <label for="pièce"> pièce </label> <br/>
-          <input type="number" name="quantite" /> <br/>
-
-          <p> Indiquez le prix que vous souhaitez </p> <br/><!-- on choisit le prix de fruit/légume qu'on dépose -->
-          <input name="prix" type="number"/> <br/>
-
-          <label for="datecuellette"> Quel est la date de cueillette de votre produit: </label>
+         <label for="datecuellette"> Quel est la date de cueillette de votre produit: </label>
           <input type ="date" name="datecueillette"/> <br/>
+
+            <div class="unite_prix">
+          <p>Indiquez la quantité que vous vouhaitez en kg ou à la pièce </p> <br/> <!-- on choisit la quantité de fruit/légume qu'on dépose --> 
+          <input type="radio" name="unite" value="kg" id="kg"/> <label for="kg"> kg </label> 
+          <input type="radio" name="unite" value="pièce" id="pièce"/> <label for="pièce"> pièce </label> <br/>
+          <input type="number" name="quantite" /> 
+          </div>
+
+          <div class="prix">
+          <p> Indiquez le prix que vous souhaitez </p> <br/><!-- on choisit le prix de fruit/légume qu'on dépose -->
+          <input name="prix" type="number"/> 
+          </div><br/>
           
-          <label  for="description" > Description du produit </label> <br/> 
-          <textarea name="description" rows="8" cols="45">
+          <label for="description" > Description du produit </label> <br/> 
+          <textarea class="description_produit" name="description" rows="8" cols="45">
           </textarea> <br/>
 
-          <label for ="echangeok"> Voulez-vous effectuer une vente et/ou un échange </label> <br/>
+          <label class ="echangeok"for ="echangeok"> Voulez-vous effectuer une vente et/ou un échange </label> <br/>
           <input type="radio" name="echangeok" value="Oui" id="echange" /> <label for="fruit">Echange</label><br />
           <input type="radio" name="echangeok" value="Non" id="vente" /> <label for="legume">Vente</label><br /> <br/>
 
@@ -75,17 +81,22 @@
           <textarea name="descriptionechange" rows="8" cols="45">
           </textarea> <br/>
 
+          <div class="versement">
           <label for ="payement"> Indiquez le versement que vous désirer </label> <br/>
-          <input type="radio" name="payement" value="carte" id="carte" /> <label for="carte">Carte</label><br />
-          <input type="radio" name="payement" value="cash" id="cash" /> <label for="cash">cash</label><br /> <br/>
+          <input type="checkbox" name="payement" value="carte" id="carte" /> <label for="carte">Carte</label><br />
+          <input type="checkbox" name="payement" value="especes" id="espece" /> <label for="especes">Espèces</label><br /> <br/>
+          </div>
 
+          <div class="type_envoie"
           <label for ="typeenvoie"> De quelle façon voulez-vous vendre/échanger votre produit </label> <br/>
           <input type ="radio" name = "typeenvoie" value="main" id="main"/> <label for="mainpropre"> En main propre </label> <br/>
           <input type ="radio" name="typeenvoie" value="poste" id="poste"/> <label for ="parposte"> Par la poste </label> <br/>
+          </div>
 
+          <div class="colis"
           <label for ="prixcolis"> Indiquez le prix du colis </label> <br/>
-          <input type="number" name="prixcolis" /> <br/>
-
+          <input type="number" name="prixcolis" /> 
+          </div><br/>
           
           <input type="submit" value="Valider" name="bouton"/>
         </form>
@@ -100,9 +111,9 @@
         $req = $bdd2->prepare('INSERT INTO produit (PR_nom,PR_unite) VALUE (?,?)');
         $req1 = $bdd2->prepare('INSERT INTO propositionechange (PR_idP,PE_photos,PE_quantite) VALUE (?,?,?)');
 
-        $sel1= $bdd2->prepare('SELECT MAX(PR_idP) FROM Produit ');
-        $sel2= $bdd2-> prepare('SELECT MAX(PE_idPropositionEchan) FROM propositionechange ');
-
+        /*$sel1= $bdd2->prepare('SELECT MAX(PR_idP) FROM Produit ');
+        $sel2= $bdd2-> prepare('SELECT MAX(PE_idPropositionEchan) FROM propositionechange ');*/
+              
         $req2 = $bdd2->prepare('INSERT INTO Annonce 
           (AN_idAnnonce, US_idUserannonceur, PR_idP, PE_idPropositionEchan, AN_quantite,AN_prix, AN_echangeok, AN_echangedescription, AN_moyentpayment,AN_moyenenvoie, AN_datepublication, AN_prixcolis, AN_description)
            VALUE (?,?,?,?,?,?,?,?,?,?,?,?,?)' );
@@ -113,14 +124,15 @@
 
 
         $req->execute(array($_POST['fruit'],$_POST['unite']));
-        $req1->execute(array($_POST['PR_idP'],'Bonjour',$_POST['quantite']));
+        $req1->execute(array($_POST[''],'Bonjour',$_POST['quantite']));
 
-         $sel1->execute();
-        $sel2->execute();
+        /*$sel1->execute();
+        $sel2->execute();*/
         
+        $last_id = $bdd2->lastInsertId();  
 
-        $req2->execute(array('','',$sel1->PR_idP,$sel2->PE_idPropositionEchan,$_POST['quantite'],$_POST['prix'],$_POST['echangeok'],
-          $_POST['descriptionechange'],$_POST['payement'],$_POST['typeenvoie'],$_POST['datecueillette'],$_POST['prixcolis'],$_POST['description']));
+        /*$req2->execute(array('','',$sel1->PR_idP,$sel2->PE_idPropositionEchan,$_POST['quantite'],$_POST['prix'],$_POST['echangeok'],
+          $_POST['descriptionechange'],$_POST['payement'],$_POST['typeenvoie'],$_POST['datecueillette'],$_POST['prixcolis'],$_POST['description']));*/
 
         
       }
