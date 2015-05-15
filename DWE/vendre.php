5d9d8aab99b927955befc64da36d806789cedec4
AltunerSinan <sinan-selcuk.altuner@isep.fr>
2015-05-15 11:37:00 +0200

    upload image fait a moitié
    upload image fait a moitié

--- DWE/vendre.php
+++ DWE/vendre.php
@@ -20,7 +20,7 @@
 
     <body>
             
-    <h1 id="VENDRE">       Vendre</h1>
+    <h1 id="VENDRE"> Vendre</h1>
         <h2>Pour mettre en vente un produit veuillez remplir le formulaire ci-dessous</h2>
             
        
@@ -31,21 +31,21 @@
 
     <section >
       <fieldset>
-        <form id="FormulaireVendre" method="post" action="vendre.php" enctype="multipart/form-data" name="form1">
+        <form id="FormulaireVendre" method="post" action="vendre.php"  name="form1">
           <strong> Veuillez indiquer si vous vendez des fruits ou des légumes:</strong><br />
 
           <input type="radio" name="TypeProduit" value="fruit" id="fruit" /> <label for="fruit">fruit</label><br />
           <input type="radio" name="TypeProduit" value="legume" id="legume" /> <label for="legume">légume</label><br /> <br/>
 
           <label for="fruit"> <strong>   Quel fruit voulez-vous déposez ?</strong>  </label> <br/> <!-- on choisit le fruit qu'on dépose -->
-          <select name="fruit" id="fruit"> 
+          <select name="fruit" id="fruit" class="inputvendre"> 
             <option value="pomme"> Pomme </option>
             <option value="poire"> Poire </option>
             <option value="fraise"> Fraise </option>
           </select> <br/><br/>
 
           <label for="legume"> <strong>  Quel legume voulez-vous déposez ? </strong></label> <br/> <!-- on choisit le légume qu'on dépose -->
-          <select name="legume" id="legume">
+          <select name="legume" id="legume" class="inputvendre">
             <option value="brocolis"> Brocolis </option>
             <option value="artichaut"> Artichaut </option>
             <option value="radis"> radis </option>
@@ -60,10 +60,10 @@
           <input name="prix" type="number" class="inputvendre"/> <br/><br/>
 
           <label for="datecuellette">  <strong>Indiquez la date de cueillette de votre produit:</strong> </label><br/>
-          <input type ="date" name="datecueillette" class="inputvendre"/> <br/><br/>
+          <input type ="date" name="datecueillette" class="inputvendre" placeholder="Date de la cueillette"/> <br/><br/>
           
           <label  for="description" > <strong> Description du produit</strong> </label> <br/> 
-          <textarea name="description" rows="8" cols="45" class="inputvendre">
+          <textarea name="description" rows="8" cols="45" class="inputvendre" >
           </textarea> <br/><br/>
 
           <label for ="echangeok"> <strong>Voulez-vous effectuer une vente et/ou un échange</strong> </label> <br/>
@@ -72,29 +72,30 @@
 
 
           <label  for="descriptionechange" > <strong>Description de l'echange</strong> </label> <br/> 
-          <textarea name="descriptionechange" rows="8" cols="45" >
+          <textarea class="inputvendre" name="descriptionechange" rows="8" cols="45" >
           </textarea> <br/><br/>
 
           <label for ="payement"> <strong> Indiquez le versement que vous désirer</strong>  </label> <br/>
           <input type="radio" name="payement" value="carte" id="carte" /> <label for="carte">Carte</label><br />
-          <input type="radio" name="payement" value="cash" id="cash" /> <label for="cash">cash</label><br /> <br/><br/>
+          <input type="radio" name="payement" value="cash" id="cash" /> <label for="cash">Cash</label><br /> <br/><br/>
 
           <label for ="typeenvoie"> <strong>De quelle façon voulez-vous vendre/échanger votre produit</strong> </label> <br/>
           <input type ="radio" name = "typeenvoie" value="main" id="main"/> <label for="mainpropre"> En main propre </label> <br/>
           <input type ="radio" name="typeenvoie" value="poste" id="poste"/> <label for ="parposte"> Par la poste </label> <br/><br/>
 
           <label for ="prixcolis"> <strong>Indiquez le prix du colis en euros </strong></label> <br/>
-          <input type="number" name="prixcolis" /> <br/> <br/>
-          <label for="icone"> <strong>Icône du produit (JPG, PNG ou GIF | max. 15 Ko) :</strong></label><br /> 
-          <input type="file" name="icone" id="icone" /><br /><br />
-          <label for="mon_fichier"> <strong>Fichier (tous formats | max. 1 Mo) :</strong></label><br />
-          <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-          <input type="file" name="mon_fichier" id="mon_fichier" /><br /> <br />
-          <label for="titre"> <strong>Titre du produit (max. 50 caractères) :</strong></label><br />
-          <input type="text" name="titre" value="Titre du fichier" id="titre" /><br /><br />
+          <input type="number" name="prixcolis"  class="inputvendre"/> <br/> <br/>
+
+
+<p> vous pouvez aussi insérer une image du produit </p>
           
-          <input type="submit" value="Valider" name="bouton" id="valider"/>
-        
+
+</form>
+
+<form id="FormulaireVendre" method="post" action="vendre.php" enctype="multipart/form-data" name="form1">
+
+<input type="file" name="file">  </br> 
+<input type="submit" value="insérer l'image"  name="submit">
 
 </form>
       </fieldset>
@@ -114,6 +115,45 @@
  
 
     <!-- PHP pour le formulaire d'envoie à la BD -->
+
+
+
+    <?php
+      if(isset($_POST['submit']))
+        {
+          $fichier = $_FILES['file']['name'];
+          $taille_maximale=2097512;
+          $taille=filesize($_FILES['file']['tmp_name']);
+          $extensions=array ('.png','.jpg','.jpeg','.PNG','.JPG','.JPEG');
+          $extension=strrchr($fichier, '.');
+
+
+        if(!in_array($extension, $extensions))
+        {
+          $error="<div class='alert'> Vous devez uploader un fichier de type png, jpg, jpeg </div>";
+        }
+
+        if($taille>$taille_maximale){
+          $error="<div class='alert'>  Le fichier est trop volumineux </div>";
+        }
+
+        if(!isset($error))
+        {
+          $fichier=preg_replace('/(^.a-z0-9]+)/i','-', $fichier);
+          move_uploaded_file($_FILES['file']['tmp_name'], "ImagesUploades/333-".$fichier);
+
+        }else{
+          echo $error;
+        }
+      
+      }
+
+    ?>
+
+
+
+
+
 
       <?php
 
