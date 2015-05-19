8e3a3b0bba33058c22620eae368157483be65bd8
AltunerSinan <sinan-selcuk.altuner@isep.fr>
2015-05-19 15:09:21 +0200

    Revert 9bc92b8..2bfb114
    This rolls back to commit 9bc92b850c85be087429be57f90c7dd63353bbea.

--- DWE/vendre.php
+++ DWE/vendre.php
@@ -13,7 +13,8 @@
         <?php include('php/config.php'); ?>
         <?php include('php/connexion.php'); ?>
         <?php include('php/header.php'); ?>
-    <?php if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?> 
+    <?php 
+    if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?> 
       
     
     </header>
@@ -33,97 +34,124 @@
         <h1 class="h1_vente"> VOTRE ANNONCE </h1>
         <form id="FormulaireVendre" method="post" action="vendre.php" enctype="multipart/form-data" name="form1">
 
-                      <label for="Produit"> Produit : </label>  <!-- on choisit le fruit qu'on dépose -->
-                      <select name="produit" id="produit">
-                          <option> Choisir </option>
-          <?php 
-              $prods=$bdd2->query('SELECT DISTINCT* FROM produit ORDER BY PR_nom ASC');
-              foreach ($prods as $prods):
-          ?>
-                          <option  value="<?php echo $prods['PR_idP']; ?>"> <?php echo $prods['PR_nom']; ?> </option>
-                              <?php endforeach; ?>
-                          <option> Autre </option>
-                      </select><br/>
-                      <label> Si autre : </label> <input type="text"  /> </br>
-                     
-                  
-                    <label for="quantite">Quantité : </label> <!-- on choisit la quantité de fruit/légume qu'on dépose --> 
-                    <input type="number" name="quantite" />
-                    <input type="radio" name="unite" value="kg" id="kg"/> <label for="kg"> kg </label> 
-                    <input type="radio" name="unite" value="pièce" id="pièce"/> <label for="pièce"> pièce </label> <br/>
+            <label for="Produit"> Produit : </label>  <!-- on choisit le fruit qu'on dépose -->
+            <select name="produit" id="produit">
+                <option> Choisir </option>
+<?php 
+    $prods=$bdd2->query('SELECT DISTINCT* FROM produit ORDER BY PR_nom ASC');
+    foreach ($prods as $prods):
+?>
+                <option  value="<?php echo $prods['PR_idP']; ?>"> <?php echo $prods['PR_nom']; ?> </option>
+                    <?php endforeach; ?>
+                <option> Autre </option>
+            </select><br/>
+            <label> Si autre : </label> <input type="text"  /> </br>
+           
+        
+          <label for="quantite">Quantité : </label> <!-- on choisit la quantité de fruit/légume qu'on dépose --> 
+          <input type="number" name="quantite" />
+          <input type="radio" name="unite" value="kg" id="kg"/> <label for="kg"> kg </label> 
+          <input type="radio" name="unite" value="pièce" id="pièce"/> <label for="pièce"> pièce </label> <br/>
 
-                    <label> Prix : </label><!-- on choisit le prix de fruit/légume qu'on dépose -->
-                    <input name="prix" type="number" class="inputvendre"/> <br/><br/>
+          <label> Prix : </label><!-- on choisit le prix de fruit/légume qu'on dépose -->
+          <input name="prix" type="number" class="inputvendre"/> <br/><br/>
 
-                    <div class="cueillette">
-                    <label for="datecueillette"> Date de cueillette : </label>
-                    <input type ="date" name="datecueillette"/><br/>
-                    </div>
-                    
-                    <label  for="description" > Description du produit : </label>
-                    <textarea name="description" rows="4" cols="45">
-                    </textarea> <br/>
-
-                  <h1 class="h1_vente"> ECHANGE </h1>
-                    <label class ="echangeok"for ="echangeok"> Type de transaction : </label>
-                    <input  type="checkbox" name="echangeok" value="Non" id="vente" /> <label for="legume">Vente</label>
-                    <input  type="checkbox" name="echangeok" value="Oui" id="echange" /> <label for="fruit">Echange</label><br />
-
-                    
-                    
-                    <label  for="descriptionechange" > Description de l'echange :</label> 
-                    <textarea class="inputvendre" name="descriptionechange" rows="8" cols="45" >
-                    </textarea> <br/><br/>
-
-                    <label for ="payement"> Versement  désiré :  </label>
-                    <input type="radio" name="payement" value="carte" id="carte" /> <label for="carte">Carte</label>
-                    <input type="radio" name="payement" value="cash" id="cash" /> <label for="cash">Cash</label><br /><br />
-
-                    <label for ="typeenvoie"> Echange : </label> 
-                    <input type ="radio" name = "typeenvoie" value="main" id="main"/> <label for="mainpropre"> En main propre </label> 
-                    <input type ="radio" name="typeenvoie" value="poste" id="poste"/> <label for ="parposte"> Par la poste </label> 
-
-                    <label for ="prixcolis"> Prix du colis : </label> 
-                    <input type="number" name="prixcolis"  class="inputvendre"/> <br/> <br/>
-                    
-          <br/> <br/>
-                    <label style="width:450px;"> Vous pouvez aussi insérer une image du produit : </label>
-                    <input type="file" name="file"> <br/>
-                    <input type="submit" value="Envoyer"  name="bouton" />
+          <div class="cueillette">
+          <label for="datecueillette"> Date de cueillette : </label>
+          <input type ="date" name="datecueillette"/><br/>
+          </div>
           
+          <label  for="description" > Description du produit : </label>
+          <textarea name="description" rows="4" cols="45">
+          </textarea> <br/>
 
+        <h1 class="h1_vente"> ECHANGE </h1>
+          <label class ="echangeok"for ="echangeok"> Type de transaction : </label>
+          <input  type="checkbox" name="echangeok" value="Non" id="vente" /> <label for="legume">Vente</label>
+          <input  type="checkbox" name="echangeok" value="Oui" id="echange" /> <label for="fruit">Echange</label><br />
+
+          
+          
+          <label  for="descriptionechange" > Description de l'echange :</label> 
+          <textarea class="inputvendre" name="descriptionechange" rows="8" cols="45" >
+          </textarea> <br/><br/>
+
+          <label for ="payement"> Versement  désirer :  </label>
+          <input type="radio" name="payement" value="carte" id="carte" /> <label for="carte">Carte</label>
+          <input type="radio" name="payement" value="espece" id="cash" /> <label for="espece">Espèce</label><br /><br />
+
+          <label for ="typeenvoie"> Echange : </label> 
+          <input type ="radio" name = "typeenvoie" value="main" id="main"/> <label for="mainpropre"> En main propre </label> 
+          <input type ="radio" name="typeenvoie" value="poste" id="poste"/> <label for ="parposte"> Par la poste </label> 
+
+          <label for ="prixcolis"> Prix du colis : </label> 
+          <input type="number" name="prixcolis"  class="inputvendre"/> <br/> <br/>
+<input type="submit" value="Envoyer"  name="bouton" />
+                </form>
+<form method="post" name="image">
+<label style="width:450px;"> Vous pouvez aussi insérer une image du produit : </label>
+<input type="file" name="file"> 
+    <input type="submit" value="Insérer photo"  name="submit">
         
 
-       </form>
+</form>
 
 
     </section>
 
-  
-
-
-
 <?php 
 
 if(isset($_POST['bouton'])){
-   
-    $selec=$bdd2->query('SELECT User.US_idUser, Annonce.US_idUserannonceur FROM User INNER JOIN Annonce ON User.US_idUser = Annonce.US_idUserannonceur');
 
+    
+   // $selec=$bdd2->query('SELECT * FROM User WHERE US_pseudo="'.$_SESSION['login'].'"');
+    //$ligne=$selec->fetch();
  
-    if($article=$bdd2->exec('INSERT INTO Annonce  VALUES ("","'.$_SESSION['id'].'","'.$_POST['produit'].'",NULL,"'.$_POST['quantite'].'","'.$_POST['prix'].'","'.$_POST['echangeok'].'","'.$_POST['descriptionechange'].'","'.$_POST['payement'].'","'.$_POST['typeenvoie'].'","'.$_POST['datecueillette'].'","'.$_POST['prixcolis'].'","'.$_POST['description'].'","'.$_SESSION['id'].'","'.$nomfichier.'")')==TRUE){
+    if($article=$bdd2->exec('INSERT INTO Annonce  VALUES ("","'.$_SESSION['id_perso'].'","'.$_POST['produit'].'",NULL,"'.$_POST['quantite'].'","'.$_POST['prix'].'","'.$_POST['echangeok'].'","'.$_POST['descriptionechange'].'","'.$_POST['payement'].'","'.$_POST['typeenvoie'].'","'.$_POST['datecueillette'].'","'.$_POST['prixcolis'].'","'.$_POST['description'].'","'.$_SESSION['id_perso'].'")')==TRUE){
 
     echo "bravo !";
     }else{ echo "no";}
 }
 
-
-
-         
 ?>
 
+ 
+
+    <!-- PHP pour le formulaire d'envoie à la BD -->
 
 
 
+    <?php
+      if(isset($_POST['submit']))
+        {
+          $fichier = $_FILES['file']['name'];
+          $taille_maximale=2097512;
+          $taille=filesize($_FILES['file']['tmp_name']);
+          $extensions=array ('.png','.jpg','.jpeg','.PNG','.JPG','.JPEG');
+          $extension=strrchr($fichier, '.');
+
+
+        if(!in_array($extension, $extensions))
+        {
+          $error="<div class='alert'> Vous devez uploader un fichier de type png, jpg, jpeg </div>";
+        }
+
+        if($taille>$taille_maximale){
+          $error="<div class='alert'>  Le fichier est trop volumineux </div>";
+        }
+
+        if(!isset($error))
+        {
+          $fichier=preg_replace('/(^.a-z0-9]+)/i','-', $fichier);
+          move_uploaded_file($_FILES['file']['tmp_name'], "ImagesUploades/333-".$fichier);
+
+        }else{
+          echo $error;
+        }
+      
+      }
+
+    ?>
 
 
 
