2ff7cf15c8547a4e00b8332e273100fd719dd99f
josephjonathan <jonathan.joseph@isep.fr>
2015-05-20 10:38:50 +0200

    Ajout reinitialisation du mdp + correction connectez vous base de donnée

---
+++ DWE/reinitialisationmdp.php
@@ -0,0 +1,17 @@
+<?php session_start(); ?>
+<html>
+    <head>
+        <meta charset=UTF-8>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+
+        <title>Deal With Eat</title>
+        
+    </head>
+    
+<div id="principal">
+    <header>
+        <?php include('php/config.php'); ?>
+        <?php include('php/connexion.php'); ?>
+        <?php include('php/header.php'); ?>
+		<?php if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?>
