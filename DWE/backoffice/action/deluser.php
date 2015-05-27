9214526268192f6df845c4d8615579910815b6cd
HugoHerp1 <herpin.h@gmail.com>
2015-05-27 12:03:44 +0200

    hug style backoff

--- DWE/backoffice/action/deluser.php
+++ DWE/backoffice/action/deluser.php
@@ -1,23 +1,28 @@
 <title>Deal With Eat - Backoffice</title>
+ <meta charset=UTF-8>
 </head>
 <header>
     <?php include('../../php/config.php'); ?>
+    <link rel="stylesheet" type="text/css" href="../backoffice.css" media="screen"/>
 </header>
+<img id = 'logo_byebye' src="../../css/images/logoDWE.png">
 
 <?php
 
 $id=$_GET['id'];
 
-    echo $id;
+   
 
                     $sql2 =' DELETE FROM User WHERE US_idUser="'.$id.'"';                
                      if($sql2query=$bdd2->exec($sql2)==true)
                      {
-                                echo 'Le membre a bien été suprimé !';
+                                echo '<center><p id="byebye_user">Le membre a bien été suprimé !</p></center>';
                                     }
                         else
                         { 
-                            echo "Probleme 2";
+                            echo '<center><p id="byebye_user">Erreur, le memrbe n\'a pas été suprimé</p></center>';
                         }
                     
+
      ?>
+     <a href="../membreadmin.php"> Retour au backoffice </a>
