8d664ab5b601e2e9f6edd1d25b64f1759685c122
josephjonathan <jonathan.joseph@isep.fr>
2015-06-03 09:42:45 +0200

    envoi mail, kelke bug a koriger

--- DWE/mailcontact.php
+++ DWE/mailcontact.php
@@ -1,64 +1,26 @@
+<?php session_start(); ?>
 <html>
-<head>
-    <meta charset=UTF-8>
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
+    <head>
+        <meta charset=UTF-8>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 
-    <title>Deal With Eat</title>
-
-</head>
-<div id="body">
-    <div id="principal">
-        <header>
-            <?php include('php/config.php'); ?>
-            <?php include('php/header.php'); ?>
-            <?php if (isset($_SESSION['id']) && $_SESSION['id'] == '1') {
-                include('nav_connect.php');
-            } else {
-                include('nav.php');
-            } ?>
+        <title>Deal With Eat</title>
+        
+    </head>
+    
+<div id="principal">
+    <header>
+        <?php include('php/config.php'); ?>
+        <?php include('php/connexion.php'); ?>
+        <?php include('php/header.php'); ?>
+		<?php if(isset($_SESSION['id']) && $_SESSION['id']=='1'){include('nav_connect.php');}else{include('nav.php');} ?>
 
 
-        </header>
-        <body>
-        <h1 id="FAQh1" style="border-bottom:2px dotted gray;width:90%;"> Contacter nous </h1>
 
-        <form id="formmail" action="mailcontact.php" style="width:60%;" method="post">
-            <input type="radio" name="x" value="un"><label style="width: 200px;" for="probl"> Problème</label>
-            <br>
-            <input type="radio" name="x" value="deux"><label style="width: 200px;" for="suggest"> Suggestion</label>
-            <br>
+ <p> votre email a bien été envoyé </p>
 
-            
-     
-
-            <br>
-            <input name="titre_mail" placeholder="Sujet de mail" required/>
-            <input name="qui" placeholder="Votre pseudo">
-            <br>
-            <br>
-            <textarea name="contenu" placeholder="Contenu du mail" rows="10" cols="100"></textarea>
-            <br>
-            <center><input type="submit" value="Envoyer le mail"></center>
-        </form>
-        <table>
-              <tr>
-                        <th > Votre pseudo </th>
-                        <th> Votre mail </th>
-                             
-                </tr>
-
-        <?php
-          if(isset($_POST['contenu'])){
-            $_SESSION['contenu']=$_POST['contenu'];
-        }
-          if (isset($_POST['qui'])){
-            $_SESSION['qui']=$_POST['qui'];
-          }
-
-        ?>
-
-        <?php date_default_timezone_set('Etc/UTC');
+ <?php date_default_timezone_set('Etc/UTC');
 
                         require 'phpmailer/PHPMailerAutoload.php';
 
@@ -120,10 +82,3 @@
                             echo "Votre message à bien été pris en compte. Nous vous recontacterons dans les plus bref delais";
                         }
                         ?>
-
-        </body>
-
-        <?php include('php/pied_de_page.php'); ?>
-
-
-</html>
